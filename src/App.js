8988dc060ffec7d3a183fa5c6230d022c7b7209a
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-08-09 19:26:04 +0200

    feat: test for first 3 features added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import CitySearch from "./components/CitySearch";
+import EventList from "./components/EventList";
+import {extractLocations, getEvents} from "./api";
+import NumberOfEvents from "./components/NumberOfEvents";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [selectedCity, setSelectedCity] = useState("See all cities");
+  const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [allLocations, setAllLocations] = useState([]);
+  const [eventNumber, setEventNumber] = useState(32);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const eventList = await getEvents();
+        setEvents(eventList);
+        setAllLocations(extractLocations(eventList));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleCitySelected = (city, numberOfEvents) => {
+    setSelectedCity(city);
+    const filteredEvents = events.filter((event) => event.location === city);
+    let sliced = [];
+    if (city === "See all cities") {
+      sliced = events.slice(0, numberOfEvents);
+    } else {
+      //use eventNumber if present to limit number
+      sliced = filteredEvents.slice(0, numberOfEvents);
+    }
+    setFilteredEvents(sliced);
+  };
+
+  function onEventNumberChange(number) {
+    console.log("onEventNumberChange:", number);
+    setEventNumber(number);
+    handleCitySelected(selectedCity, number);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: "#f04908"}}>
+      <CitySearch
+        allLocations={allLocations}
+        setSelectedCity={handleCitySelected}
+      />
+      <NumberOfEvents
+        eventNumber={eventNumber}
+        onEventNumberChange={onEventNumberChange}
+      />
+      <EventList events={filteredEvents.length > 0 ? filteredEvents : events} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/EventList.js
@@ -0,0 +1,13 @@
+import Event from "./Event";
+
+const EventList = ({events}) => {
+  return (
+    <ul id="event-list">
+      {events
+        ? events.map((event, index) => <Event event={event} key={index} />)
+        : null}
+    </ul>
+  );
+};
+
+export default EventList;
